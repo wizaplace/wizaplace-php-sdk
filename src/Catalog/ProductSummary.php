5bc664fb8cb9a8c7810b2080a8e7d66785ecfe7b
hectorj <hector.jusforgues@gmail.com>
2017-05-29 16:13:24 +0200

    Fix PHPDoc

--- src/Catalog/ProductSummary.php
+++ src/Catalog/ProductSummary.php
@@ -42,7 +42,7 @@ class ProductSummary
     private $condition;
     /** @var SearchProductAttribute[] */
     private $attributes;
-    /** @var SearchCategoryPath[] */
+    /** @var ProductCategory[] */
     private $categoryPath;
     /** @var string */
     private $slug;
@@ -168,7 +168,7 @@ class ProductSummary
     }
 
     /**
-     * @return SearchCategoryPath[]
+     * @return ProductCategory[]
      */
     public function getCategoryPath(): array
     {
