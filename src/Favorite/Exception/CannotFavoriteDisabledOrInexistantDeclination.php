a7eebd99b374f32fcc3bf57da9c6a131921f02ae
hectorj <hector.jusforgues@gmail.com>
2017-06-19 11:21:08 +0200

    Favorites: improve exception handling

--- src/Favorite/Exception/CannotFavoriteDisabledOrInexistantDeclination.php
+++ src/Favorite/Exception/CannotFavoriteDisabledOrInexistantDeclination.php
@@ -9,4 +9,14 @@ namespace Wizaplace\Favorite\Exception;
 
 class CannotFavoriteDisabledOrInexistantDeclination extends \Exception
 {
+    public const HTTP_ERROR_CODE = 409;
+
+    public function __construct(int $productId, \Throwable $previous = null)
+    {
+        parent::__construct(
+            "Product #{$productId} cannot be added to favorite as it is disabled or does not exist",
+            self::HTTP_ERROR_CODE,
+            $previous
+        );
+    }
 }
--- src/Favorite/Exception/FavoriteAlreadyExist.php
+++ src/Favorite/Exception/FavoriteAlreadyExist.php
@@ -9,4 +9,11 @@ namespace Wizaplace\Favorite\Exception;
 
 class FavoriteAlreadyExist extends \Exception
 {
+    public const HTTP_ERROR_CODE = 400;
+
+    public function __construct(int $productId, \Throwable $previous = null)
+    {
+        parent::__construct("Product #{$productId} is already a favorite", self::HTTP_ERROR_CODE, $previous);
+    }
+
 }
