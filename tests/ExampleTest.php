4a856ca4a4cab9fc3a4039eb1b424316aa7bf941
hectorj <hector.jusforgues@gmail.com>
2017-06-23 15:16:59 +0200

    Add authentication example

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -20,15 +20,42 @@ class ExampleTest extends ApiTestCase
      */
     public function basicUsage()
     {
+        // Setup
         $marketplaceApiUri = 'http://wizaplace.loc/api/v1/'; // replace that value with your own
         $httpClient = new \GuzzleHttp\Client([
             'base_uri' => $marketplaceApiUri,
         ]);
         $wizaplaceClient = new \Wizaplace\ApiClient($httpClient);
         $catalogService = new \Wizaplace\Catalog\CatalogService($wizaplaceClient);
+
+        // Search
         $products = $catalogService->search();
 
         // Just here so PHPUnit does not complain about the lack of assertions
         $this->assertNotEmpty($products);
+    }
+
+    /**
+     * Shows how to use a service which requires authentication.
+     * @test
+     */
+    public function authenticationUsage()
+    {
+        // Setup
+        $marketplaceApiUri = 'http://wizaplace.loc/api/v1/'; // replace that value with your own
+        $httpClient = new \GuzzleHttp\Client([
+            'base_uri' => $marketplaceApiUri,
+        ]);
+        $wizaplaceClient = new \Wizaplace\ApiClient($httpClient);
+        $orderService = new \Wizaplace\Order\OrderService($wizaplaceClient);
+
+        // Authentication
+        $wizaplaceClient->authenticate("admin@wizaplace.com", "password");
+
+        // Authenticated Action
+        $orders = $orderService->getOrders();
+
+        // Just here so PHPUnit does not complain about the lack of assertions
+        $this->assertEmpty($orders);
     }
 }
