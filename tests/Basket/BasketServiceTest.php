af0b9b40fa8fd218c9c06d12170ed662721cf25a
hectorj <hector.jusforgues@gmail.com>
2017-07-06 14:55:56 +0200

    BasketService: add method to select shippings

--- tests/Basket/BasketServiceTest.php
+++ tests/Basket/BasketServiceTest.php
@@ -24,6 +24,53 @@ class BasketServiceTest extends ApiTestCase
         $this->assertNotNull($basket);
     }
 
+    public function testSelectingShippings()
+    {
+        // Arrange
+        $basketService = $this->buildAuthenticatedBasketService();
+
+        $basketId = $basketService->create();
+        $this->assertNotEmpty($basketId);
+
+        $basketService->addProductToBasket($basketId, '5', 1);
+
+        $basket = $basketService->getBasket($basketId);
+        $this->assertNotNull($basket);
+
+        $companyGroups = $basket->getCompanyGroups();
+        $this->assertCount(1, $companyGroups);
+
+        $shippingGroups = $companyGroups[0]->getShippingGroups();
+        $this->assertCount(1, $shippingGroups);
+
+        $shippings = $shippingGroups[0]->getShippings();
+        $this->assertCount(2, $shippings);
+
+        $this->assertTrue($shippings[0]->isSelected());
+        $this->assertFalse($shippings[1]->isSelected());
+
+        // Act
+        $basketService->selectShippings($basketId, [
+            $shippingGroups[0]->getId() => $shippings[1]->getId(),
+        ]);
+
+        // Assert
+        $basket = $basketService->getBasket($basketId);
+        $this->assertNotNull($basket);
+
+        $companyGroups = $basket->getCompanyGroups();
+        $this->assertCount(1, $companyGroups);
+
+        $shippingGroups = $companyGroups[0]->getShippingGroups();
+        $this->assertCount(1, $shippingGroups);
+
+        $shippings = $shippingGroups[0]->getShippings();
+        $this->assertCount(2, $shippings);
+
+        $this->assertFalse($shippings[0]->isSelected());
+        $this->assertTrue($shippings[1]->isSelected());
+    }
+
     private function buildAuthenticatedBasketService(string $email = "admin@wizaplace.com", string $password = "password"): BasketService
     {
         $apiClient = $this->buildApiClient();
