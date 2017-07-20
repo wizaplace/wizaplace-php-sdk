616e465fc769914216a4b29f176dce333d8ce725
Serge Mazille <serge@wizacha.com>
2017-07-20 16:45:19 +0200

    Added Wizaplace\User creation test

---
+++ tests/User/UserServiceTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author      Wizacha DevTeam <dev@wizacha.com>
+ * @copyright   Copyright (c) Wizacha
+ * @license     Proprietary
+ */
+declare(strict_types = 1);
+
+namespace Wizaplace\Tests\User;
+
+use Wizaplace\User\UserService;
+use Wizaplace\Tests\ApiTestCase;
+
+class UserServiceTest extends ApiTestCase
+{
+    public function testCreateUser()
+    {
+        $userEmail = 'user@example.com';
+        $userPassword = 'password';
+
+        // authenticate
+        $client = $this->buildApiClient();
+        $userService = new UserService($client);
+
+        // create new user
+        $userId = (int) $userService->register($userEmail, $userPassword);
+
+        // authenticate with newly created user
+        $client->authenticate($userEmail, $userPassword);
+
+        // fetch user
+        $user = $userService->getProfileFromId($userId);
+
+        $this->assertNotNull($user, 'User exists');
+        $this->assertEquals($user->getEmail(), $userEmail);
+    }
+}
