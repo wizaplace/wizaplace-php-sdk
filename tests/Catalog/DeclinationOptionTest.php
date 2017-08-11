6fbb0e00c8c6406b2ff52fa1a1b748f843e2c0c5
hectorj <hector.jusforgues@gmail.com>
2017-08-11 11:35:32 +0200

    add simple tests on JSON serialization of our classes already implementing \JsonSerializable

---
+++ tests/Catalog/DeclinationOptionTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author      Wizacha DevTeam <dev@wizacha.com>
+ * @copyright   Copyright (c) Wizacha
+ * @license     Proprietary
+ */
+declare(strict_types = 1);
+
+namespace Wizaplace\Tests\Catalog;
+
+use PHPUnit\Framework\TestCase;
+use Wizaplace\Catalog\DeclinationOption;
+
+class DeclinationOptionTest extends TestCase
+{
+    public function testDeclinationOptionJsonSerialization()
+    {
+        $result = \json_encode(new DeclinationOption([
+            'id' => 1,
+            'name' => 'size',
+            'variantId' => 1,
+            'variantName' => 'xs',
+        ]), JSON_PRETTY_PRINT);
+
+        $expected = <<<JSON
+{
+    "id": 1,
+    "name": "size",
+    "variantId": 1,
+    "variantName": "xs"
+}
+JSON;
+        $this->assertSame($expected, $result);
+    }
+}
