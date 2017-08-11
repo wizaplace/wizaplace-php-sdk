40710056a65c2bacc08d0b4bb553d5589e7aa9b2
Baptiste Langlade <baptiste@wizacha.com>
2017-08-11 09:50:55 +0200

    add docblock on new files

--- src/Catalog/DeclinationSummary.php
+++ src/Catalog/DeclinationSummary.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @author      Wizacha DevTeam <dev@wizacha.com>
+ * @copyright   Copyright (c) Wizacha
+ * @license     Proprietary
+ */
 declare(strict_types = 1);
 
 namespace Wizaplace\Catalog;
