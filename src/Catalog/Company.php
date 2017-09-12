46c249dcf8132d8385619b9f98fa7b066f254b4e
hectorj <hector.jusforgues@gmail.com>
2017-09-12 11:54:12 +0200

    Add missing company fields

--- src/Catalog/Company.php
+++ src/Catalog/Company.php
@@ -7,21 +7,42 @@ declare(strict_types = 1);
 
 namespace Wizaplace\SDK\Catalog;
 
+use Wizaplace\SDK\Image\Image;
+
+/**
+ * @deprecated all usages will be replaced by {@see \Wizaplace\SDK\Catalog\CompanySummary} in v0.9
+ */
 final class Company
 {
-    /** @var integer */
+    /** @var int */
     private $id;
 
     /** @var string */
     private $name;
 
+    /** @var string */
+    private $slug;
+
+    /** @var null|Image */
+    private $image;
+
+    /** @var bool */
+    private $professional;
+
+    /** @var float|null */
+    private $averageRating;
+
     /**
      * @internal
      */
-    public function __construct($data)
+    public function __construct(array $data)
     {
         $this->id = $data['id'];
         $this->name = $data['name'];
+        $this->slug = $data['slug'];
+        $this->image = isset($data['image']) ? new Image($data['image']) : null;
+        $this->professional = $data['isProfessional'];
+        $this->averageRating = $data['averageRating'] ?? null;
     }
 
     public function getId(): int
@@ -32,5 +53,25 @@ final class Company
     public function getName(): string
     {
         return $this->name;
+    }
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    public function getImage(): ?Image
+    {
+        return $this->image;
+    }
+
+    public function isProfessional(): bool
+    {
+        return $this->professional;
+    }
+
+    public function getAverageRating(): ?float
+    {
+        return $this->averageRating;
     }
 }
