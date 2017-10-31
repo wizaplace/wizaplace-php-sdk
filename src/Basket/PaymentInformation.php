483d6bfb69477ac2be43d109d45ba673db326b8c
hectorj <hector.jusforgues@gmail.com>
2017-10-31 14:41:30 +0100

    Basket\PaymentInformation::getOrders now returns an array of Basket\BasketOrder instead of an array of undocumented associative arrays
    Closes #204

--- src/Basket/PaymentInformation.php
+++ src/Basket/PaymentInformation.php
@@ -12,7 +12,7 @@ use Psr\Http\Message\UriInterface;
 
 final class PaymentInformation
 {
-    /** @var array */
+    /** @var BasketOrder[] */
     private $orders;
     /** @var null|UriInterface */
     private $redirectUrl;
@@ -24,11 +24,16 @@ final class PaymentInformation
      */
     public function __construct(array $data)
     {
-        $this->orders = $data['orders'];
+        $this->orders = array_map(static function (array $orderData): BasketOrder {
+            return new BasketOrder($orderData);
+        }, $data['orders']);
         $this->redirectUrl = !isset($data['redirectUrl']) || $data['redirectUrl'] === '' ? null : new Uri($this->redirectUrl);
         $this->html = $data['html'] ?? '';
     }
 
+    /**
+     * @return BasketOrder[]
+     */
     public function getOrders(): array
     {
         return $this->orders;
