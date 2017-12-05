013bd2ed79b902d9bcf1ed828c92a9ccd1fcd11e
Hector Jusforgues <hector.jusforgues@gmail.com>
2017-12-05 10:02:53 +0100

    Add Pim/ProductService::listProducts (#224)
    * Add Pim/ProductService::listProducts
    
    and associated classes
    
    * put the calcul of nbPages in its own private method, for readability
    
    * write testListProductsWithStatusFilter and fix bug it revealed
    
    * write testListProductsWithMultipleFilters
    
    * fix code style

--- src/Pim/Product/ProductService.php
+++ src/Pim/Product/ProductService.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace Wizaplace\SDK\Pim\Product;
 
+use GuzzleHttp\RequestOptions;
 use Wizaplace\SDK\AbstractService;
 
 final class ProductService extends AbstractService
@@ -17,5 +18,24 @@ final class ProductService extends AbstractService
         $data = $this->client->get("products/$productId");
 
         return new Product($data);
+    }
+
+    public function listProducts(?ProductListFilter $filter = null, $page = 1, $itemsCountPerPage = 100): ProductList
+    {
+        $this->client->mustBeAuthenticated();
+        $query = [
+            'page' => $page,
+            'items_per_page' => $itemsCountPerPage,
+        ];
+
+        if ($filter) {
+            $query = array_merge($query, $filter->toArray());
+        }
+
+        $data = $this->client->get('products', [
+            RequestOptions::QUERY => $query,
+        ]);
+
+        return new ProductList($data);
     }
 }
--- tests/Pim/Product/ProductServiceTest.php
+++ tests/Pim/Product/ProductServiceTest.php
@@ -7,14 +7,17 @@ declare(strict_types=1);
 
 namespace Wizaplace\SDK\Tests\Pim\Product;
 
+use Wizaplace\SDK\Pagination;
 use Wizaplace\SDK\Pim\Product\Product;
 use Wizaplace\SDK\Pim\Product\ProductApprovalStatus;
 use Wizaplace\SDK\Pim\Product\ProductAttachment;
 use Wizaplace\SDK\Pim\Product\ProductDeclination;
 use Wizaplace\SDK\Pim\Product\ProductGeolocation;
 use Wizaplace\SDK\Pim\Product\ProductImage;
+use Wizaplace\SDK\Pim\Product\ProductListFilter;
 use Wizaplace\SDK\Pim\Product\ProductService;
 use Wizaplace\SDK\Pim\Product\ProductStatus;
+use Wizaplace\SDK\Pim\Product\ProductSummary;
 use Wizaplace\SDK\Tests\ApiTestCase;
 
 final class ProductServiceTest extends ApiTestCase
@@ -107,7 +110,7 @@ final class ProductServiceTest extends ApiTestCase
         $this->assertCount(2, $attachments);
 
         $attachment = $attachments[0];
-        $this->assertSame('c33533cc-54c5-4fe5-8dbb-5f62ba3d3927', $attachment->getId());
+        $this->assertNotEmpty($attachment->getId());
         $this->assertSame('Manuel de montage', $attachment->getLabel());
     }
 
@@ -123,6 +126,254 @@ final class ProductServiceTest extends ApiTestCase
         $this->assertSame('69009', $geolocation->getZipcode());
         $this->assertSame(45.778848, $geolocation->getLatitude());
         $this->assertSame(4.800039, $geolocation->getLongitude());
+    }
+
+    public function testListProductsWithDefaultArgs()
+    {
+        $result = $this->buildProductService()->listProducts();
+        $products = $result->getProducts();
+        $this->assertContainsOnly(ProductSummary::class, $products);
+        $this->assertLessThanOrEqual(100, count($products));
+        $this->assertGreaterThan(0, count($products));
+
+        $pagination = $result->getPagination();
+        $this->assertInstanceOf(Pagination::class, $pagination);
+        $this->assertSame(1, $pagination->getPage());
+        $this->assertSame(100, $pagination->getResultsPerPage());
+        $this->assertSame(9, $pagination->getNbResults());
+        $this->assertSame(1, $pagination->getNbPages());
+
+        foreach ($products as $product) {
+            $this->assertGreaterThan(0, $product->getId());
+            $this->assertNotEmpty($product->getCode());
+            $this->assertNotEmpty($product->getName());
+            $this->assertGreaterThan(0, $product->getMainCategoryId());
+            $this->assertGreaterThan(1500000000, $product->getCreatedAt()->getTimestamp());
+            $this->assertGreaterThanOrEqual($product->getCreatedAt()->getTimestamp(), $product->getLastUpdateAt()->getTimestamp());
+            $this->assertGreaterThan(0, $product->getCompanyId());
+        }
+    }
+
+    public function testListProductWithGeolocation()
+    {
+        $filter = (new ProductListFilter())->byProductCode('20230495445');
+        $products = $this->buildProductService()->listProducts($filter)->getProducts();
+        $this->assertContainsOnly(ProductSummary::class, $products);
+        $this->assertCount(1, $products);
+
+        $geolocation = $products[0]->getGeolocation();
+        $this->assertInstanceOf(ProductGeolocation::class, $geolocation);
+
+        $this->assertSame('Wizacha', $geolocation->getLabel());
+        $this->assertSame('69009', $geolocation->getZipcode());
+        $this->assertSame(45.778848, $geolocation->getLatitude());
+        $this->assertSame(4.800039, $geolocation->getLongitude());
+    }
+
+    public function testListProductWithAttachments()
+    {
+        $filter = (new ProductListFilter())->byProductCode('20230495446');
+        $products = $this->buildProductService()->listProducts($filter)->getProducts();
+        $this->assertContainsOnly(ProductSummary::class, $products);
+        $this->assertCount(1, $products);
+
+        $attachments = $products[0]->getAttachments();
+        $this->assertContainsOnly(ProductAttachment::class, $attachments);
+        $this->assertCount(2, $attachments);
+
+        $attachment = $attachments[0];
+        $this->assertNotEmpty($attachment->getId());
+        $this->assertSame('Manuel de montage', $attachment->getLabel());
+    }
+
+    public function testListProductPagination()
+    {
+        $result1 = $this->buildProductService()->listProducts(null, 1, 1);
+        $productsPage1 = $result1->getProducts();
+        $this->assertContainsOnly(ProductSummary::class, $productsPage1);
+        $this->assertCount(1, $productsPage1);
+        $paginationPage1 = $result1->getPagination();
+        $this->assertInstanceOf(Pagination::class, $paginationPage1);
+        $this->assertSame(1, $paginationPage1->getPage());
+        $this->assertSame(1, $paginationPage1->getResultsPerPage());
+        $this->assertSame(9, $paginationPage1->getNbResults());
+        $this->assertSame(9, $paginationPage1->getNbPages());
+
+        $result2 = $this->buildProductService()->listProducts(null, 2, 1);
+        $productsPage2 = $result2->getProducts();
+        $this->assertContainsOnly(ProductSummary::class, $productsPage2);
+        $this->assertCount(1, $productsPage2);
+        $paginationPage2 = $result2->getPagination();
+        $this->assertInstanceOf(Pagination::class, $paginationPage2);
+        $this->assertSame(2, $paginationPage2->getPage());
+        $this->assertSame(1, $paginationPage2->getResultsPerPage());
+        $this->assertSame(9, $paginationPage2->getNbResults());
+        $this->assertSame(9, $paginationPage2->getNbPages());
+
+        $this->assertNotEquals($productsPage2[0]->getId(), $productsPage1[0]->getId());
+
+        $result = $this->buildProductService()->listProducts(null, 1, 2);
+        $products = $result->getProducts();
+        $this->assertContainsOnly(ProductSummary::class, $productsPage1);
+        $this->assertCount(1, $productsPage1);
+        $this->assertSame($productsPage1[0]->getId(), $products[0]->getId());
+        $this->assertSame($productsPage2[0]->getId(), $products[1]->getId());
+        $pagination = $result->getPagination();
+        $this->assertInstanceOf(Pagination::class, $pagination);
+        $this->assertSame(1, $pagination->getPage());
+        $this->assertSame(2, $pagination->getResultsPerPage());
+        $this->assertSame(9, $pagination->getNbResults());
+        $this->assertSame(5, $pagination->getNbPages());
+    }
+
+    public function testListProductsWithCategoryFilter()
+    {
+        $filter = (new ProductListFilter())->byCategoryIds([3], false);
+        $products = $this->buildProductService()->listProducts($filter)->getProducts();
+        $this->assertContainsOnly(ProductSummary::class, $products);
+        $this->assertGreaterThanOrEqual(2, count($products));
+
+        foreach ($products as $product) {
+            $this->assertSame(3, $product->getMainCategoryId());
+        }
+    }
+
+    public function testListProductsWithCategoryAndSubCategoriesFilter()
+    {
+        $filter = (new ProductListFilter())->byCategoryIds([3], true);
+        $products = $this->buildProductService()->listProducts($filter)->getProducts();
+        $this->assertContainsOnly(ProductSummary::class, $products);
+        $this->assertGreaterThanOrEqual(2, count($products));
+
+        $categoriesIds = [];
+        foreach ($products as $product) {
+            $categoriesIds[$product->getMainCategoryId()] = true;
+        }
+
+        $this->assertSame([
+            3 => true,
+            4 => true,
+        ], $categoriesIds);
+    }
+
+    /**
+     * @dataProvider statusProvider
+     */
+    public function testListProductsWithStatusFilter(ProductStatus $status, int $minimumExpectedCount)
+    {
+        $filter = (new ProductListFilter())->byStatus($status);
+        $products = $this->buildProductService()->listProducts($filter)->getProducts();
+        $this->assertContainsOnly(ProductSummary::class, $products);
+        $this->assertGreaterThanOrEqual($minimumExpectedCount, count($products));
+
+        foreach ($products as $product) {
+            $this->assertTrue($status->equals($product->getStatus()));
+        }
+    }
+
+    /**
+     * @see \Wizaplace\SDK\Tests\Pim\Product\ProductServiceTest::testListProductsWithStatusFilter
+     */
+    public function statusProvider(): array
+    {
+        return [
+            'enabled' => [ProductStatus::ENABLED(), 2],
+            'hidden' => [ProductStatus::HIDDEN(), 0], // @TODO: add hidden products in fixtures
+            'disabled' => [ProductStatus::DISABLED(), 0], // @TODO: add disabled products in fixtures
+        ];
+    }
+
+    /**
+     * @dataProvider multiFilterProvider
+     */
+    public function testListProductsWithMultipleFilters(
+        ?ProductStatus $status = null,
+        ?array $categoryIds = null,
+        bool $includeSubCategories,
+        ?array $expectedCategoryIds,
+        ?string $productCode = null,
+        int $minimumExpectedCount = 1
+    ) {
+        $filter = new ProductListFilter();
+        if ($status !== null) {
+            $filter->byStatus($status);
+        }
+        if ($categoryIds !== null) {
+            $filter->byCategoryIds($categoryIds, $includeSubCategories);
+        }
+        if ($productCode !== null) {
+            $filter->byProductCode($productCode);
+        }
+
+        $products = $this->buildProductService()->listProducts($filter)->getProducts();
+        $this->assertContainsOnly(ProductSummary::class, $products);
+        $this->assertGreaterThanOrEqual($minimumExpectedCount, count($products));
+
+        $categoriesIds = [];
+        foreach ($products as $product) {
+            $categoriesIds[$product->getMainCategoryId()] = true;
+            if ($status !== null) {
+                $this->assertTrue($status->equals($product->getStatus()));
+            }
+            if ($productCode !== null) {
+                $this->assertSame($productCode, $product->getCode());
+            }
+        }
+
+        if ($expectedCategoryIds !== null) {
+            $this->assertSame($expectedCategoryIds, $categoriesIds);
+        }
+    }
+
+    public function multiFilterProvider(): array
+    {
+        return [
+            'enabled, in a specific category or its subcategories' => [
+                ProductStatus::ENABLED(),
+                [3],
+                true,
+                [
+                    3 => true,
+                    4 => true,
+                ],
+                null,
+                2,
+            ],
+            'enabled, in a specific category or its subcategories, with a specific product code' => [
+                ProductStatus::ENABLED(),
+                [3],
+                true,
+                [
+                    4 => true,
+                ],
+                '0000001',
+                1,
+            ],
+            'disabled, in a specific category or its subcategories' => [
+                ProductStatus::DISABLED(),
+                [3],
+                true,
+                [],
+                null,
+                0,
+            ],
+            'enabled, with a specific product code' => [
+                ProductStatus::ENABLED(),
+                null,
+                false,
+                null,
+                '20230495445',
+                1,
+            ],
+            'disabled, with a specific product code which is enabled' => [
+                ProductStatus::DISABLED(),
+                null,
+                false,
+                [],
+                '20230495445',
+                0,
+            ],
+        ];
     }
 
     private function buildProductService($userEmail = 'admin@wizaplace.com', $userPassword = 'password'): ProductService
