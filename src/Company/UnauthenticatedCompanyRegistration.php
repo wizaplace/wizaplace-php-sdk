d51e0a9af5125b9476339142460a90d0d19e6ee9
Nicolas Gomes <nicolas@wizaplace.com>
2017-12-13 14:56:24 +0100

    change namespace

--- src/Company/UnauthenticatedCompanyRegistration.php
+++ src/Company/UnauthenticatedCompanyRegistration.php
@@ -5,9 +5,7 @@
  */
 declare(strict_types = 1);
 
-namespace Wizaplace\Company;
-
-use Wizaplace\SDK\Company\CompanyRegistration;
+namespace Wizaplace\SDK\Company;
 
 final class UnauthenticatedCompanyRegistration extends CompanyRegistration
 {
