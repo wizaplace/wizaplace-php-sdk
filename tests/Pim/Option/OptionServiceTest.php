3dcd22ccef4cbe8bbe7637633e2dfbde0fca2772
Hector Jusforgues <hector.jusforgues@gmail.com>
2017-12-14 09:55:29 +0100

    Add Pim\Product\Option\OptionService::getCategoryOptions (#244)
    * Add Pim\Product\Option\OptionService::getCategoryOptions
    
    * remove isPendingModeration
    
    * move namespace Pim/Product/Option to Pim/Option

---
+++ tests/Pim/Option/OptionServiceTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @copyright Copyright (c) Wizacha
+ * @license Proprietary
+ */
+declare(strict_types=1);
+
+namespace Wizaplace\SDK\Tests\Pim\Option;
+
+use Wizaplace\SDK\Pim\Option\Option;
+use Wizaplace\SDK\Pim\Option\OptionService;
+use Wizaplace\SDK\Pim\Option\OptionVariant;
+use Wizaplace\SDK\Tests\ApiTestCase;
+
+class OptionServiceTest extends ApiTestCase
+{
+    public function testGetCategoryOptions()
+    {
+        $options = $this->buildOptionService()->getCategoryOptions(4);
+
+        $this->assertContainsOnly(Option::class, $options);
+        $this->assertGreaterThanOrEqual(4, count($options));
+
+        foreach ($options as $option) {
+            $this->assertGreaterThan(0, $option->getId());
+            $this->assertNotEmpty($option->getName());
+            $variants = $option->getVariants();
+            $this->assertContainsOnly(OptionVariant::class, $variants);
+
+            foreach ($variants as $variant) {
+                $this->assertGreaterThan(0, $variant->getId());
+                $this->assertNotEmpty($variant->getName());
+            }
+        }
+    }
+
+    private function buildOptionService($userEmail = 'admin@wizaplace.com', $userPassword = 'password'): OptionService
+    {
+        $apiClient = $this->buildApiClient();
+        $apiClient->authenticate($userEmail, $userPassword);
+
+        return new OptionService($apiClient);
+    }
+}
