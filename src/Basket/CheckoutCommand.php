6a502ba338b61bb9e51b32aebfaeec5a4ec3f7ec
Nicolas Gomes De Oliveira <ngdo27@gmail.com>
2018-03-06 10:58:17 +0100

    create new Checkout Commands, with an interface

---
+++ src/Basket/CheckoutCommand.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @copyright Copyright (c) Wizacha
+ * @license Proprietary
+ */
+declare(strict_types = 1);
+
+namespace Wizaplace\SDK\Basket;
+
+use Wizaplace\SDK\Exception\SomeParametersAreInvalid;
+
+class CheckoutCommand implements CheckoutCommandInterface
+{
+    /** @var string|null */
+    protected $basketId;
+
+    /** @var int|null */
+    protected $paymentId;
+
+    /** @var bool|null */
+    protected $acceptTerms;
+
+    public function getBasketId(): ?string
+    {
+        return $this->basketId;
+    }
+
+    public function setBasketId(?string $basketId): void
+    {
+        $this->basketId = $basketId;
+    }
+
+    public function getPaymentId(): ?int
+    {
+        return $this->paymentId;
+    }
+
+    public function setPaymentId(?int $paymentId): void
+    {
+        $this->paymentId = $paymentId;
+    }
+
+    public function isAcceptTerms(): bool
+    {
+        return $this->acceptTerms;
+    }
+
+    public function setAcceptTerms(?bool $acceptTerms): void
+    {
+        $this->acceptTerms = $acceptTerms;
+    }
+
+    /**
+     * @throws SomeParametersAreInvalid
+     */
+    public function validate(): void
+    {
+        if (!isset($this->basketId)) {
+            throw new SomeParametersAreInvalid('Missing basket ID');
+        }
+
+        if (!isset($this->paymentId)) {
+            throw new SomeParametersAreInvalid('Missing payment ID');
+        }
+
+        if (!isset($this->acceptTerms)) {
+            throw new SomeParametersAreInvalid('Missing terms acceptance');
+        }
+    }
+
+    public function serialize(): array
+    {
+        return [
+            'paymentId'                 => $this->paymentId,
+            'acceptTermsAndConditions'  => $this->acceptTerms,
+        ];
+    }
+}
---
+++ src/Basket/CheckoutWithPreAuthTokenCommand.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @copyright   Copyright (c) Wizacha
+ * @license     Proprietary
+ */
+declare(strict_types=1);
+
+namespace Wizaplace\SDK\Basket;
+
+use Wizaplace\SDK\Exception\SomeParametersAreInvalid;
+
+final class CheckoutWithPreauthTokenCommand extends CheckoutCommand
+{
+    /** @var string */
+    private $preauthToken;
+
+    public function getPreauthToken(): string
+    {
+        return $this->preauthToken;
+    }
+
+    public function setPreauthToken(string $preauthToken): void
+    {
+        $this->preauthToken = $preauthToken;
+    }
+
+    public function validate(): void
+    {
+        parent::validate();
+        if (!isset($this->preauthToken)) {
+            throw new SomeParametersAreInvalid('Missing preauthToken');
+        }
+    }
+
+    public function serialize(): array
+    {
+        $serializedCheckout = parent::serialize();
+        $serializedCheckout['preauthToken'] = $this->preauthToken;
+
+        return $serializedCheckout;
+    }
+}
