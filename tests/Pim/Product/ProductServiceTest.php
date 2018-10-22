08bd037df33a9abecea84a55be9b35f619626042
Jérémy Reynaud <jeremy.reynaud@wizacha.com>
2018-10-22 11:58:17 +0200

    add method getShippings

--- tests/Pim/Product/ProductServiceTest.php
+++ tests/Pim/Product/ProductServiceTest.php
@@ -822,6 +822,15 @@ final class ProductServiceTest extends ApiTestCase
         $this->assertInstanceOf(Shipping::class, $shipping);
     }
 
+    public function testGetProductShippings()
+    {
+        $shippings = $this->buildProductService()->getShippings(5);
+
+        foreach ($shippings as $shipping) {
+            $this->assertInstanceOf(Shipping::class, $shipping);
+        }
+    }
+
     public function testPutProductShipping()
     {
         $command = new UpdateShippingCommand();
