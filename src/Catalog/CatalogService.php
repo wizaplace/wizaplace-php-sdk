e1e885acd1f2c598f47a6e487d4dd628b98618ea
Cédric Bertolini <41571181+wizacedric@users.noreply.github.com>
2019-07-01 09:33:52 +0200

    Update php-sdk/src/Catalog/CatalogService.php
    Co-Authored-By: Jérémy <42959493+jeremy-wizaplace@users.noreply.github.com>

--- src/Catalog/CatalogService.php
+++ src/Catalog/CatalogService.php
@@ -129,7 +129,7 @@ final class CatalogService extends AbstractService implements CatalogServiceInte
      */
     public function getProductsByMvpId(string $mvpId, bool $allowMvp = true) : array
     {
-        $response = $this->client->get('catalog/products', [RequestOptions::QUERY => ['mvpId' => $mvpId, 'allowMvp' => $allowMvp ? 1 : 0]]);
+        $response = $this->client->get('catalog/products', [RequestOptions::QUERY => ['mvpId' => $mvpId, 'allowMvp' => (int) $allowMvp]]);
 
         return array_map(function ($product) {
             return new Product($product, $this->client->getBaseUri());
