e6810b76733c2cf5210772889de6303be2c78931
bassem.bellaaj <bassem.bellaaj-ext@wizacha.com>
2020-01-23 17:29:38 +0100

    TMA-52 fix test sdk update

--- tests/Vendor/Order/OrderServiceTest.php
+++ tests/Vendor/Order/OrderServiceTest.php
@@ -689,8 +689,5 @@ class OrderServiceTest extends ApiTestCase
 
         static::assertInstanceOf(OrderAddress::class, $shippingAddress);
         static::assertSame('University of Southern California', $shippingAddress->getCompany());
-
-
-
     }
 }
