36a1969c694988a36eea01527c64af5f44059e4f
Aurelien Rasselet <aurelien.rasselet@wizacha.com>
2020-01-31 13:56:01 +0100

    Add exim sdk

---
+++ src/Exim/EximService.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @author      Wizacha DevTeam <dev@wizacha.com>
+ * @copyright   Copyright (c) Wizacha
+ * @license     Proprietary
+ */
+
+use GuzzleHttp\Psr7\Stream;
+use GuzzleHttp\RequestOptions;
+use Wizaplace\SDK\AbstractService;
+use Wizaplace\SDK\Authentication\AuthenticationRequired;
+use Wizaplace\SDK\Exception\FileNotFound;
+
+declare(strict_types=1);
+
+/**
+ * Class EximService
+ * @package Wizaplace\SDK\Exim
+ */
+final class EximService extends AbstractService
+{
+    /**
+     * @throws AuthenticationRequired
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Wizaplace\SDK\Exception\FileNotFound
+     * @throws \Wizaplace\SDK\Exception\JsonDecodingError
+     */
+    public function importProducts(string $filePath): string
+    {
+        $this->client->mustBeAuthenticated();
+
+        // Open CSV file
+        $file = fopen($filePath, 'r+');
+        if(false === $file) {
+            throw new FileNotFound('File not found', ['file' => $filePath]);
+        }
+
+        // Add stream to HTTP body
+        $stream = new Stream($file);
+        $data[] = [
+            'name'     => 'file',
+            'contents' => $stream,
+            'filename' => 'product.csv',
+        ];
+
+        // Send CSV file to API
+        $data = $this->client->post('import/products', [
+            RequestOptions::MULTIPART => $data,
+        ]);
+
+        return $data['jobId'];
+    }
+}
