9bec48de182b603373c35f02f838fa44b4f43df3
Thomas Léone <54528231+thomas-wizaplace@users.noreply.github.com>
2020-02-24 10:21:37 +0100

    wiz-6259 delete mark as paid status from sdk on refund (#8904)

--- src/Order/RefundStatus.php
+++ src/Order/RefundStatus.php
@@ -14,12 +14,10 @@ use MyCLabs\Enum\Enum;
  * @method static RefundStatus CREATED()
  * @method static RefundStatus PAID()
  * @method static RefundStatus FAILED()
- * @method static RefundStatus MARKED_AS_PAID()
  */
 final class RefundStatus extends Enum
 {
     private const CREATED = 'created';
     private const PAID = 'paid';
     private const FAILED = 'failed';
-    private const MARKED_AS_PAID = 'marked_as_paid';
 }
