8a7d37c45f5f6ef3af9a780d2f9b362419ff9656
Eric COURTIAL <eric.courtial@wizacha.com>
2021-11-10 17:13:30 +0100

    WIZ-10408 PHPCBF

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -357,7 +357,7 @@ final class ApiClient
         $this->addAuth($options);
 
         // Default accepted content is JSON (most of the endpoints are working with this content type)
-        if (false === array_key_exists('Accept', $options[RequestOptions::HEADERS])) {
+        if (false === \array_key_exists('Accept', $options[RequestOptions::HEADERS])) {
             $options[RequestOptions::HEADERS]['Accept'] = 'application/json';
         }
 
